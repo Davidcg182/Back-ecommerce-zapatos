e9941334c6802a5390edec96b8b8f30b48cfe24b
Davidcg182 <davidcasti182@gmail.com>
2023-01-26 14:09:01 -0500

    bug const server

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const { Server } = require("socket.io");
 const http = require("http")
 const cors = require("cors")
 
-const server = http.createServer(app)
 const app = express()
+const server = http.createServer(app)
 const port = process.env.PORT || 3001
 const io = new Server(server, {
     cors: {
